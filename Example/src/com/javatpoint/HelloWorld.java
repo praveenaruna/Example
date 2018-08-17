42df548f5394ca0f5f00b50ffc23e548a909146f
praveenaruna <praveen.lvr7@gmail.com>
2018-08-18 00:29:15 +0530

    Initial checking of example

---
+++ Example/src/com/javatpoint/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.javatpoint;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hi");
+	}
+
+	
+}
+
